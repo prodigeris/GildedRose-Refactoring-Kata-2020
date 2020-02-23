6ed920666f6d8b10929fd70599c9423a914c38ac
Arnas Kazlauskas <arnkzl@telesoftas.com>
2020-02-24 01:38:26 +0200

    feat: introduce text test generator

--- fixtures/texttest_fixture.php
+++ fixtures/texttest_fixture.php
@@ -17,7 +17,7 @@ $items = array(
     new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
     new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
     // this conjured item does not work properly yet
-    new Item('Conjured Mana Cake', 3, 6)
+//    new Item('Conjured Mana Cake', 3, 6)
 );
 
 $app = new GildedRose($items);
@@ -27,12 +27,13 @@ if (count($argv) > 1) {
     $days = (int) $argv[1];
 }
 
+$results = [];
+
 for ($i = 0; $i < $days; $i++) {
-    echo("-------- day $i --------\n");
-    echo("name, sellIn, quality\n");
     foreach ($items as $item) {
-        echo $item . PHP_EOL;
+        $results['day_'.$i][] = (string) $item;
     }
-    echo PHP_EOL;
     $app->updateQuality();
 }
+
+file_put_contents('fixtures/results.txt', json_encode($results));
